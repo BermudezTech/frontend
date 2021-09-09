2bc83427bad7405872f862ae4890f801eb01624d
JBermudezG <jebermudez587@gmail.com>
2021-09-08 20:04:43 -0500

    New folder: folletos

---
+++ folletos/travelGuru/script.js
@@ -0,0 +1,21 @@
+function nextPage() {
+  document.getElementById("vista1").style.display = "none";
+  document.getElementById("vista2").style.display = "grid";
+  document.getElementById("next").style.display = "none";
+  document.getElementById("previous").style.display = "block";
+}
+function previousPage() {
+  document.getElementById("vista1").style.display = "grid";
+  document.getElementById("vista2").style.display = "none";
+  document.getElementById("previous").style.display = "none";
+  document.getElementById("next").style.display = "block";
+}
+function showModal(name) {
+  document.getElementById(name).style.display = "flex";
+}
+function closeModal(name) {
+  document.getElementById(name).style.display = "none";
+}
+function cargada() {
+  document.getElementById("cargada").style.display = "none";
+}
