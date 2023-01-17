5ab14f9ca4f701c454583f66a66054faa1bb51c9
JBermudezG <jebermudez587@gmail.com>
2023-01-17 08:55:47 -0500

    Added responsive design

--- facebookUIClone/script.js
+++ facebookUIClone/script.js
@@ -41,27 +41,27 @@ let menuShorcuts = [
     {
         name: "Agar.io",
         location: 0,
-        backgroundImage: "https://scontent.fbog4-1.fna.fbcdn.net/v/t39.2081-6/11891365_726141290823610_1062843466_n.png?stp=c6.6.31.31a_dst-png_p36x36&_nc_cat=1&ccb=1-7&_nc_sid=eaa83b&_nc_ohc=bzQT_q0vwxQAX-sCwnX&_nc_ht=scontent.fbog4-1.fna&oh=00_AfCZHYa-oNqPCg9575E4KV4Rc-0bM2Gt_5cMQlNOAlj0CQ&oe=63C6018A"
+        backgroundImage: "img/gameIcons/agar.png"
     },
     {
         name: "Angry Birds Friends",
         location: 0,
-        backgroundImage: "https://scontent.fbog4-1.fna.fbcdn.net/v/t39.2081-6/37826797_1419176584850865_8749714633905930240_n.png?stp=c6.6.31.31a_dst-png_p36x36&_nc_cat=1&ccb=1-7&_nc_sid=eaa83b&_nc_ohc=0hT7m1WMitcAX_IuerL&_nc_ht=scontent.fbog4-1.fna&oh=00_AfAnezQ-H5L0WJ02Hjg30m5ccI9V6R1ycl0H3AYno9M7Rw&oe=63C4A1F7",
+        backgroundImage: "img/gameIcons/angryBirdsFriends.png",
     },
     {
         name: "Candy Crush Saga",
         location: 0,
-        backgroundImage: "https://scontent.fbog4-1.fna.fbcdn.net/v/t39.2081-6/22088589_1654976451201719_5191478939503034368_n.png?stp=c6.6.31.31a_dst-png_p36x36&_nc_cat=1&ccb=1-7&_nc_sid=eaa83b&_nc_ohc=El876jJQymsAX9dbXzd&_nc_ht=scontent.fbog4-1.fna&oh=00_AfC1PP8jIOiKdmosL3UDZs5B2J3KKHvoq5n4Xx5JUMSE5w&oe=63C50582"
+        backgroundImage: "img/gameIcons/candyCrushSaga.jpg"
     },
     {
         name: "Criminal Case",
         location: 0,
-        backgroundImage: "https://scontent.fbog4-1.fna.fbcdn.net/v/t39.2081-6/12057001_427250474066399_2006674606_n.png?stp=c6.6.31.31a_dst-png_p36x36&_nc_cat=1&ccb=1-7&_nc_sid=eaa83b&_nc_ohc=Q1u5pUG0ZQoAX96BMdM&_nc_ht=scontent.fbog4-1.fna&oh=00_AfAjIcVE1nZtIRHh2R1_X4gjIkhcpclF6R8SkogkV7MKGw&oe=63C6027C"
+        backgroundImage: "img/gameIcons/criminalCase.png"
     },
     {
         name: "Dragon City",
         location: 0,
-        backgroundImage: "https://scontent.fbog4-1.fna.fbcdn.net/v/t39.2081-6/28129135_1686685264729832_3956709029115330560_n.png?stp=c6.6.31.31a_dst-png_p36x36&_nc_cat=1&ccb=1-7&_nc_sid=eaa83b&_nc_ohc=Geor3I2YNAUAX-5_xP-&_nc_ht=scontent.fbog4-1.fna&oh=00_AfA0ecPg8Luv4uzxWltQ-bscb0fapuJ-PZhvCmm8aRh5bA&oe=63C4B869"
+        backgroundImage: "img/gameIcons/dragonCity.jpeg"
     },
     {
         name: "Ver más",
